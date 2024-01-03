a2ca6d3f618b9b161cdda3cd49b444baccd3025b
kelvinirungu <kelvinn.irungu69@gmail.com>
2024-01-03 12:16:52 +0300

    Passing the userData via props to the parent component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import AddUser from "./components/Users/AddUser";
 import './App.css';
 
 function App() {
+
+  const pullUserData = (userData) => (
+  console.log(userData)     
+  )
+console.log(localStorage );
   return (
-    <div className="text-center text-cyan-400">
-<h1>Hello World</h1>
+    <div className="bg-gray-600 h-[100vh] justify-center text-center text-cyan-400 pt-[10vh]">
+      <AddUser userInputData ={pullUserData}/>
     </div>
   );
 }
