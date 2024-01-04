9ee5fa920140951c92ee282d271e338df1914f96
kelvinirungu <kelvinn.irungu69@gmail.com>
2024-01-04 19:21:22 +0300

    Storing data in local storage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Userlist/UserList";
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const pullUserData = (userData) => {
+    console.log(userData, 'pullUserData')
+    
+    setItems(userData)
+  }
 
-  const pullUserData = (userData) => (
-  console.log(userData)     
-  )
+    // Store userData in localStorage
+  useEffect(() =>{
+    localStorage.setItem('userData', JSON.stringify(items));
+  },[items])
+ 
 console.log(localStorage );
   return (
     <div className="bg-gray-600 h-[100vh] justify-center text-center text-cyan-400 pt-[10vh]">
       <AddUser userInputData ={pullUserData}/>
+      <UserList/>
     </div>
   );
 }
