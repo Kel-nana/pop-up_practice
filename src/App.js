a35d6847ffeed94578da8553e33f2bb0548e750d
kelvinirungu <kelvinn.irungu69@gmail.com>
2024-01-05 17:34:01 +0300

    Changes App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Userlist/UserList";
 import './App.css';
@@ -6,20 +6,14 @@ import './App.css';
 function App() {
   const [items, setItems] = useState([]);
   const pullUserData = (userData) => {
-    console.log(userData, 'pullUserData')
-    
     setItems(userData)
   }
 
-    // Store userData in localStorage
-  useEffect(() =>{
-    localStorage.setItem('userData', JSON.stringify(items));
-  },[items])
+  console.log(items, 'app js items')
  
-console.log(localStorage );
   return (
     <div className="bg-gray-600 h-[100vh] justify-center text-center text-cyan-400 pt-[10vh]">
-      <AddUser userInputData ={pullUserData}/>
+      <AddUser userInputData ={pullUserData} />
       <UserList/>
     </div>
   );
